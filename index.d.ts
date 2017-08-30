656f815c5e4ac6037d036d1c2c16c47524f997bb
Matej <dobrakmato@gmail.com>
2017-08-30 22:44:59 +0200

    add TypeScripts typings

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Loads all configuration files from specified directory (or default 'config'
+ * directory if no argument is specified) and returns merged configuration
+ * files as single object.
+ *
+ * @type {String} directory - directory path to directory with configuration files
+ *
+ * @return {object} all configuration files merged to single object
+ */
+export = function loadConfig(directory) {
+};
